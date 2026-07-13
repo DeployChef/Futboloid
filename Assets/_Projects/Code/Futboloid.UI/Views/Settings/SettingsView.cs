a74f17c88e6dfd8556274ead32f360283271da5c
Valkn <Valknut29@gmail.com>
2026-07-13 18:29:44 +0300

    Настройки звука+ обновленный ui

--- Assets/_Projects/Code/Futboloid.UI/Views/Settings/SettingsView.cs
+++ Assets/_Projects/Code/Futboloid.UI/Views/Settings/SettingsView.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Futboloid.Core.Audio;
 using Futboloid.Core.Localization;
 using Futboloid.UI;
@@ -11,9 +12,7 @@ namespace Futboloid.UI.Views.Settings
     public class SettingsView : MonoBehaviour, IWidget
     {
         [SerializeField] private Button closeButton;
-        [SerializeField] private Button englishButton;
-        [SerializeField] private Button russianButton;
-        [SerializeField] private TextMeshProUGUI currentLanguageLabel;
+        [SerializeField] private TMP_Dropdown localeDropdown;
 
         [Header("Volume")]
         [SerializeField] private Slider musicVolumeSlider;
@@ -21,6 +20,8 @@ namespace Futboloid.UI.Views.Settings
 
         private ILocalizationService _localization;
         private IAudioManager _audio;
+        private readonly List<LocaleOption> _localeOptions = new();
+        private bool _isUpdatingDropdown;
 
         [Inject]
         public void Construct(ILocalizationService localization, IAudioManager audio)
@@ -36,11 +37,8 @@ namespace Futboloid.UI.Views.Settings
             if (closeButton != null)
                 closeButton.onClick.AddListener(Close);
 
-            if (englishButton != null)
-                englishButton.onClick.AddListener(() => SelectLocale(LocaleCodes.English));
-
-            if (russianButton != null)
-                russianButton.onClick.AddListener(() => SelectLocale(LocaleCodes.Russian));
+            if (localeDropdown != null)
+                localeDropdown.onValueChanged.AddListener(OnLocaleDropdownChanged);
 
             if (musicVolumeSlider != null)
                 musicVolumeSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
@@ -66,11 +64,8 @@ namespace Futboloid.UI.Views.Settings
             if (closeButton != null)
                 closeButton.onClick.RemoveListener(Close);
 
-            if (englishButton != null)
-                englishButton.onClick.RemoveAllListeners();
-
-            if (russianButton != null)
-                russianButton.onClick.RemoveAllListeners();
+            if (localeDropdown != null)
+                localeDropdown.onValueChanged.RemoveListener(OnLocaleDropdownChanged);
 
             if (musicVolumeSlider != null)
                 musicVolumeSlider.onValueChanged.RemoveListener(OnMusicVolumeChanged);
@@ -87,40 +82,55 @@ namespace Futboloid.UI.Views.Settings
 
         public void Close() => gameObject.SetActive(false);
 
-        private void SelectLocale(string localeCode)
-        {
-            _localization?.SetLocale(localeCode);
-            Refresh();
-        }
-
         private void Refresh()
         {
-            RefreshLanguage();
+            RefreshLocaleDropdown();
             RefreshVolumeSliders();
         }
 
-        private void RefreshLanguage()
+        private void RefreshLocaleDropdown()
         {
-            if (_localization == null)
+            if (_localization == null || localeDropdown == null)
                 return;
 
-            var currentCode = _localization.CurrentLocaleCode;
-            SetSelected(englishButton, currentCode == LocaleCodes.English);
-            SetSelected(russianButton, currentCode == LocaleCodes.Russian);
+            _isUpdatingDropdown = true;
 
-            if (currentLanguageLabel == null)
-                return;
-
-            foreach (var option in _localization.AvailableLocales)
+            // Заполняем dropdown опциями один раз
+            if (localeDropdown.options.Count == 0)
             {
-                if (option.Code != currentCode)
-                    continue;
+                _localeOptions.Clear();
+                var options = new List<TMP_Dropdown.OptionData>();
 
-                currentLanguageLabel.text = option.DisplayName;
-                return;
+                foreach (var locale in _localization.AvailableLocales)
+                {
+                    _localeOptions.Add(locale);
+                    options.Add(new TMP_Dropdown.OptionData(locale.DisplayName));
+                }
+
+                localeDropdown.options = options;
             }
 
-            currentLanguageLabel.text = currentCode;
+            // Выбираем текущую локаль
+            var currentCode = _localization.CurrentLocaleCode;
+            for (var i = 0; i < _localeOptions.Count; i++)
+            {
+                if (_localeOptions[i].Code == currentCode)
+                {
+                    localeDropdown.SetValueWithoutNotify(i);
+                    break;
+                }
+            }
+
+            _isUpdatingDropdown = false;
+        }
+
+        private void OnLocaleDropdownChanged(int index)
+        {
+            if (_isUpdatingDropdown)
+                return;
+
+            if (index >= 0 && index < _localeOptions.Count)
+                _localization?.SetLocale(_localeOptions[index].Code);
         }
 
         private void RefreshVolumeSliders()
@@ -143,17 +153,6 @@ namespace Futboloid.UI.Views.Settings
         private void OnSfxVolumeChanged(float value)
         {
             _audio?.SetSfxVolume(value);
-        }
-
-        private static void SetSelected(Button button, bool selected)
-        {
-            if (button == null)
-                return;
-
-            if (button.targetGraphic != null)
-                button.targetGraphic.color = selected
-                    ? new Color(0.85f, 1f, 0.85f, 1f)
-                    : Color.white;
         }
     }
 }
